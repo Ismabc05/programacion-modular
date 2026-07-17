c0be589ef979e415c552954d0ad483a1aee1ab30
Ismabc05 <ismaelbedmarcejas@gmail.com>
2026-07-17 11:35:37 +0200

    uso de useclass y usevalue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+const API_KEY = '12345678';
+const API_KEY_PROD = 'PROD734575';
 
 @Module({
   imports: [UsersModule, ProductsModule],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+  ],
 })
 export class AppModule {}
