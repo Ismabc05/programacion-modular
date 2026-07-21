1a8b438309fc9f077613c36b83049182ffaae86e
Ismabc05 <ismaelbedmarcejas@gmail.com>
2026-07-21 11:40:29 +0200

    actualizacion de cors

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
   const documentFactory = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, documentFactory);
 
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
